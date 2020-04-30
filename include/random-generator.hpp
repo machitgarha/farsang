be46e754e33bc4130c4d8030ed7fa55895930506
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-04-30 11:10:59 +0430

    Add basic needed stuff to RandomGenerator class

--- include/random-generator.hpp
+++ include/random-generator.hpp
@@ -1,11 +1,19 @@
 #ifndef GSLER_RANDOM_GERERATOR_INC
 #define GSLER_RANDOM_GERERATOR_INC
 
+#include <gsl/gsl_rng.h>
+
 namespace Gsler
 {
     class RandomGenerator
     {
+        public:
+            RandomGenerator();
+            ~RandomGenerator();
 
+        private:
+            gsl_rng *generator;
+            gsl_rng_type *generatorType;
     };
 }
 
