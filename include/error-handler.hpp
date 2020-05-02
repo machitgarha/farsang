ddeecad24f372e122fb65675615c808b380005f9
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-05-02 10:58:04 +0430

    Improve interface of ErrorHandler

--- include/error-handler.hpp
+++ include/error-handler.hpp
@@ -1,11 +1,24 @@
 #ifndef GSLER_ERROR_HANDLER_INC
 #define GSLER_ERROR_HANDLER_INC
 
+#include <gsl/gsl_errno.h>
+
 namespace Gsler
 {
     class ErrorHandler
     {
-        
+        public:
+            ErrorHandler() = delete;
+            ErrorHandler(const ErrorHandler &) = delete;
+            ErrorHandler(const ErrorHandler &&) = delete;
+            ~ErrorHandler() = delete;
+
+            static void checkErrors();
+
+        private:
+            // For preparing before main
+            static const bool _prepare;
+            static bool prepare() noexcept(true);
     };
 }
 
