9bfcbe9ae1133f51cd181f7cde59384a52a2c840
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-05-05 16:02:49 +0430

    Complete the RandomDistribution class

--- include/random-distribution.hpp
+++ include/random-distribution.hpp
@@ -1,10 +1,22 @@
 #ifndef GSLER_RANDOM_DISTRIBUTION_INC
 #define GSLER_RANDOM_DISTRIBUTION_INC
 
+#include "random-generator.hpp"
+
 namespace Gsler
 {
     class RandomDistribution
     {
+        public:
+            RandomDistribution() = delete;
+            ~RandomDistribution() = delete;
+
+            RandomDistribution(const RandomGenerator &);
+
+            virtual const RandomGenerator &getGenerator() const noexcept final;
+
+        private:
+            const RandomGenerator &generator;
     };
 }
 
--- src/random-distribution.cpp
+++ src/random-distribution.cpp
@@ -1,3 +1,13 @@
 #include "random-distribution.hpp"
 
 using namespace Gsler;
+
+RandomDistribution::RandomDistribution(const RandomGenerator &rGenerator):
+    generator(rGenerator)
+{
+}
+
+const RandomGenerator &RandomDistribution::getGenerator() const noexcept
+{
+    return this->generator;
+}
